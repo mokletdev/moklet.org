351b080684e2788ccc709e24f104e3f776ccf7e4
Ahsan <ahsanaz461@gmail.com>
2024-03-07 15:45:01 +0700

    fix: Remove border on hover

--- src/app/components/global/Navbar.tsx
+++ src/app/components/global/Navbar.tsx
@@ -26,7 +26,7 @@ export default function Navbar() {
           <Link
             key={navOption.title}
             href={navOption.href}
-            className={`hover:text-primary-400 w-1/5 rounded-full border border-white py-2 text-center backdrop-blur-lg transition-all duration-300 hover:border-neutral-400 ${pathname === navOption.href ? "text-red-400" : ""}`}
+            className={`hover:text-primary-400 w-1/5 rounded-full border border-white py-2 text-center backdrop-blur-lg transition-all duration-300 ${pathname === navOption.href ? "text-red-400" : ""}`}
           >
             {navOption.title}
           </Link>
