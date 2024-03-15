ee943953f454b0a7738554ea15d3e97a04642e8c
Ahsan <ahsanaz461@gmail.com>
2024-03-16 01:02:29 +0700

    refactor: Move the reusable texts element

---
+++ src/app/components/global/Text.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+
+interface TextProps {
+  children?: ReactNode;
+  className?: string;
+}
+
+export function H1({ children, className }: Readonly<TextProps>) {
+  return (
+    <h1 className={"text-[44px] font-bold leading-[140%] " + className}>
+      {children}
+    </h1>
+  );
+}
+
+export function P({ children, className }: Readonly<TextProps>) {
+  return <p className={"text-neutral-500 " + className}>{children}</p>;
+}
+
+export function UnderlineTitle({
+  children,
+  className,
+  lineWidth,
+}: Readonly<TextProps & { lineWidth: string | number }>) {
+  return (
+    <div className="relative">
+      <h2
+        className={
+          "absolute z-10 w-[1192px] text-4xl font-bold text-black " + className
+        }
+      >
+        {children}
+      </h2>
+      <span
+        className={`absolute left-[3px] top-[25px] z-0 h-[19px] w-[${lineWidth}] bg-primary-100`}
+      ></span>
+    </div>
+  );
+}
