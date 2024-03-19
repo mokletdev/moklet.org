4d099fec89f33684897c9904d6b48410059b3e7c
ben <me@benspace.xyz>
2024-03-19 07:09:00 +0700

    feat: create post queries and  tag query utils

---
+++ src/utils/database/tag.query.ts
@@ -0,0 +1,29 @@
+import prisma from "@/lib/prisma";
+
+export const findAllTag = () => {
+  return prisma.tag.findMany({
+    include: {
+      _count: {
+        select: { posts: { where: { published: true } } },
+      },
+    },
+  });
+};
+
+export const findPostByTag = async (tagName: string, published?: boolean) => {
+  const findTag = await prisma.tag.findUnique({
+    where: { tagName },
+    select: {
+      posts: {
+        orderBy: { published_at: "desc" },
+        where: { published },
+        include: {
+          user: { select: { name: true, user_pic: true } },
+          tags: true,
+        },
+      },
+    },
+  });
+
+  return findTag?.posts;
+};
