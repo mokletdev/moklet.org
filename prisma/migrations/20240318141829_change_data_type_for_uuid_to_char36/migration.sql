97fbd02185c9b43129095bcecb869d6d793aa770
ben <me@benspace.xyz>
2024-03-19 07:01:03 +0700

    fix(database): make slug on post unique

---
+++ prisma/migrations/20240318141829_change_data_type_for_uuid_to_char36/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - The primary key for the `Submission` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to alter the column `submission_id` on the `Submission` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Char(36)`.
+  - The primary key for the `Submission_Field` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to alter the column `submission_field_id` on the `Submission_Field` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Char(36)`.
+
+*/
+-- AlterTable
