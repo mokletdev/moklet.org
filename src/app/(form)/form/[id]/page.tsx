2cbea9235d5ce8599d56fb6a1b2bd50c77702128
FadhilKholaf <fadhilgd123@gmail.com>
2024-03-21 11:41:53 +0700

    feat: create form

--- src/app/(form)/form/[id]/page.tsx
+++ src/app/(form)/form/[id]/page.tsx
@@ -1,23 +1,22 @@
 import { PrimaryButton } from "@/app/_components/global/Button";
 import { TextArea, TextField } from "@/app/_components/global/Input";
-import { H2, P } from "@/app/_components/global/Text";
+import { H1, H2, P } from "@/app/_components/global/Text";
 import { SectionWrapper } from "@/app/_components/global/Wrapper";
 import EclipseIcon from "@/app/_components/icons/EclipseIcon";
 import React from "react";
+import { findForm } from "@/utils/database/form.query";
 
-const page = () => {
+const page = async ({ params }: { params: { id: string } }) => {
+  var form = await findForm({ id: params.id });
+  if (!form) return <H1>Not found</H1>;
+  console.log(form);
+
   return (
     <SectionWrapper id="form">
       <div className="relative flex flex-col w-full items-start justify-between gap-[54px]">
         <div className="w-full">
-          <H2 className="mb-[18px]">
-            Sampaikan Aspirasimu untuk Masa Depan yang Lebih Baik
-          </H2>
-          <P>
-            Aspirasi mewujudkan impian dan memberi ruang tumbuh bagi harapan.
-            Aspirasi Anda sebagai siswa akan membantu MPK Moklet untuk
-            mengembangkan program-programnya.
-          </P>
+          <H2 className="mb-[18px]">{form.title}</H2>
+          <P>{form.description}</P>
         </div>
         <form className="block w-full">
           <TextField
--- src/types/entityRelations.ts
+++ src/types/entityRelations.ts
@@ -3,3 +3,7 @@ import { Prisma } from "@prisma/client";
 export type PostWithTagsAndUser = Prisma.PostGetPayload<{
   include: { tags: true; user: true };
 }>;
+
+export type FormWithFields = Prisma.FormGetPayload<{
+  include: { fields: { include: { options: true } } };
+}>;
