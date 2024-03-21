7d739890e32c7ce27eb3ad42cc95cc3dfb47dd7c
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-03-21 21:48:15 +0700

    feat(frontend): Add SLA and AUP pages, Add ToS section to Footer

---
+++ src/app/(main)/policies/tos/acceptable-use-policy/page.tsx
@@ -0,0 +1,10 @@
+import { AupText } from "../_components/policiesText";
+import { SectionWrapper } from "@/app/_components/global/Wrapper";
+
+export default function Aup() {
+  return (
+    <SectionWrapper id="aup">
+      <AupText />
+    </SectionWrapper>
+  );
+}
