cff4141294cbed52022e9758f6f10e4e9ff0d8de
Ahsan <ahsanaz461@gmail.com>
2024-03-24 00:10:58 +0700

    feat(frontend): UX optimizations

--- src/app/(main)/berita/components/SearchBar.tsx
+++ src/app/(main)/berita/components/SearchBar.tsx
@@ -1,35 +1,40 @@
 "use client";
 
-import { SmallSectionWrapper } from "@/app/_components/global/Wrapper";
 import { TextField } from "@/app/_components/global/Input";
-import { SearchButton, SmallSearchButton } from "./parts/Button";
-import Search from "./icons/Search";
-import { useState } from "react";
+import { SmallSectionWrapper } from "@/app/_components/global/Wrapper";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Search from "./icons/Search";
+import { SearchButton, SmallSearchButton } from "./parts/Button";
 
-export function SearchBar() {
+export function SearchBar({ query: q }: { query?: string }) {
   const { replace } = useRouter();
-  const [Query, setQuery] = useState<string>("");
-  function handleSearch(q: string) {
-    setQuery(q);
+  const [query, setQuery] = useState<string>(q ?? "");
+  function handleSearch(query: string) {
+    setQuery(query);
   }
 
   return (
     <SmallSectionWrapper id="SearchBar">
       <div className="flex gap-3 justify-center items-center w-full mt-[30px] md:mt-0">
         <TextField
-          placeholder="Temukan berita terbaru di sini..."
+          placeholder="Cari berita di moklet.org..."
           className="w-[318px] md:w-[1049px]"
-          value={Query}
+          value={query}
           handleChange={(e) => {
             handleSearch(e.target.value);
           }}
           type={""}
           name={""}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              replace(`/berita/search?q=${query}`);
+            }
+          }}
         />
         <SmallSearchButton
           onClick={() => {
-            replace(`/berita/search?q=${Query}`);
+            replace(`/berita/search?q=${query}`);
           }}
         >
           <Search />
@@ -37,7 +42,7 @@ export function SearchBar() {
         <SearchButton
           isDisabled={false}
           onClick={() => {
-            replace(`/berita/search?q=${Query}`);
+            replace(`/berita/search?q=${query}`);
           }}
         >
           Cari berita
