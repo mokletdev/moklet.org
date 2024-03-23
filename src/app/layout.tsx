e5c4c26fa51508afc235891154e395b1b087eb75
ben <me@benspace.xyz>
2024-03-24 05:41:44 +0700

    feat: setup sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "./_components/main/NextAuthProvider";
+import NextTopLoader from "nextjs-toploader";
+import { Toaster } from "sonner";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -22,6 +24,13 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={montserrat.className}>
+        <Toaster duration={3000} pauseWhenPageIsHidden theme="light" />
+        <NextTopLoader
+          color="#E04E4E"
+          showSpinner={false}
+          crawl
+          easing="ease"
+        />
         <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
