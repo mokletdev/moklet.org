4b447fd18b552a1e5a3f6586435c64bb6aaf2cfe
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-03-23 10:51:13 +0700

    modified:   src/app/(main)/berita/components/parts/Button.tsx

--- src/app/(main)/berita/components/parts/Button.tsx
+++ src/app/(main)/berita/components/parts/Button.tsx
@@ -2,21 +2,20 @@ import Link from "next/link";
 import { MouseEventHandler, ReactNode } from "react";
 
 interface ButtonProps {
-    children?: ReactNode;
-    href: string;
-  }
-  
+  children?: ReactNode;
+  href: string;
+}
 
-  export default function PrimaryLinkButton({
-    children,
-    href,
-  }: Readonly<ButtonProps>) {
-    return (
-      <Link
-        href={href}
-        className="inline-block w-fit rounded-full bg-primary-400 px-[13px] py-[13px] transition-all duration-500 hover:bg-primary-200"
-      >
-        <span className="text-base text-white">{children}</span>
-      </Link>
-    );
-  }
+export default function PrimaryLinkButton({
+  children,
+  href,
+}: Readonly<ButtonProps>) {
+  return (
+    <Link
+      href={href}
+      className="inline-block w-fit rounded-full bg-primary-400 px-[13px] py-[13px] transition-all duration-500 hover:bg-primary-200"
+    >
+      <span className="text-base text-white">{children}</span>
+    </Link>
+  );
+}
