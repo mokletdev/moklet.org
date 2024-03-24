3e13b6096e6d7853ce4a5718762e5713a1c07ccc
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-03-24 17:22:22 +0700

    Fix(frontend): Implement responsive Developer page

--- src/app/(main)/developer/_components/DeveloperFigure.tsx
+++ src/app/(main)/developer/_components/DeveloperFigure.tsx
@@ -11,30 +11,30 @@ interface DeveloperData {
 
 export default function DeveloperFigure({ dev }: { dev: DeveloperData }) {
   return (
-    <figure className="w-[372px] flex flex-col gap-[32px]">
-      <div className="flex flex-col gap-2">
+    <figure className="w-full lg:w-[29.5%] xl:w-[372px] flex flex-col gap-[32px] text-wrap">
+      <div className="flex flex-col gap-2 text-wrap">
         <H4>{dev.name}</H4>
         <P>{dev.role}</P>
       </div>
       <div className="w-full h-0 border border-black"></div>
-      <div className="w-full h-[278px]">
+      <div className="w-full h-[278px] lg xl:h-[278px] lg:h-[183px]">
         <Image
           src={dev.picture}
           alt={"waw"}
           width={372}
           height={278}
-          className="w-full h-[278px]"
+          className="w-full h-full"
           unoptimized
         />
       </div>
       <div>
-        <div className="flex gap-[46px] mb-2">
+        <div className="flex gap-[34px] mb-2 flex-row lg:flex-col lg:gap-0 xl:gap-[46px] xl:flex-row">
           <span className="w-[84px] text-neutral-500 font-[14px]">
             Instagram
           </span>
           <span className="text-black font-normal">{dev.instagram}</span>
         </div>
-        <div className="flex gap-[46px]">
+        <div className="flex gap-[34px] mb-2 flex-row lg:flex-col lg:gap-0 xl:gap-[46px] xl:flex-row">
           <span className="w-[84px] text-neutral-500 font-[14px]">Email</span>
           <span className="text-black font-normal">{dev.email}</span>
         </div>
