a976184bf283b1cd26e7a982eb99d0884cd962db
Ahsan <ahsanaz461@gmail.com>
2024-03-24 16:39:11 +0700

    feat(frontend): Optimize searching mechanism

--- src/app/(main)/berita/[slug]/_components/BackButton.tsx
+++ src/app/(main)/berita/[slug]/_components/BackButton.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { BackButton } from "../../components/parts/Button";
-import ArrowLeft from "../../components/icons/ArrowLeft";
+import { BackButton } from "../../_components/parts/Button";
+import ArrowLeft from "../../_components/icons/ArrowLeft";
 import { default as useRouter } from "@/utils/customRouter";
 
 export default function GoBack() {
