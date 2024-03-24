25c9b0428639acf87eb0f9942ac16aa3c0eb29b9
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-03-24 17:04:52 +0700

    Feat(frontend): Implement Developer page

--- src/app/(main)/developer/page.tsx
+++ src/app/(main)/developer/page.tsx
@@ -1,24 +1,74 @@
 import Image from "@/app/_components/global/Image";
 import Link from "next/link";
+import { SectionWrapper } from "@/app/_components/global/Wrapper";
+import { H1, P } from "@/app/_components/global/Text";
+import DeveloperFigure from "./_components/DeveloperFigure";
+
+const devs = [
+  {
+    name:"Ambatron",
+    role:"Penyefonk",
+    picture: "https://pbs.twimg.com/media/GHK3FYNa8AAR21N.jpg",
+    instagram: "@loremipsum",
+    email:"drimibul@ambatukam.com"
+  },
+  {
+    name:"Adit",
+    role:"Waw",
+    picture: "https://pbs.twimg.com/media/GHK3FYNa8AAR21N.jpg",
+    instagram: "@loremipsum",
+    email:"drimibul@ambatukam.com"
+  },
+  {
+    name:"Ahsan",
+    role:"CEO",
+    picture: "https://pbs.twimg.com/media/GHK3FYNa8AAR21N.jpg",
+    instagram: "@loremipsum",
+    email:"drimibul@ambatukam.com"
+  },
+  {
+    name:"Bayu",
+    role:"Numpang nama",
+    picture: "https://pbs.twimg.com/media/GHK3FYNa8AAR21N.jpg",
+    instagram: "@loremipsum",
+    email:"drimibul@ambatukam.com"
+  },
+  {
+    name:"Gilang",
+    role:"🥶",
+    picture: "https://pbs.twimg.com/media/GHK3FYNa8AAR21N.jpg",
+    instagram: "@loremipsum",
+    email:"drimibul@ambatukam.com"
+  },
+  {
+    name:"Beni",
+    role:"Hengker",
+    picture: "https://pbs.twimg.com/media/GHK3FYNa8AAR21N.jpg",
+    instagram: "@loremipsum",
+    email:"drimibul@ambatukam.com"
+  },
+]
 
 export default function Developer() {
   return (
-    <>
-      <section className="min-h-[744px] w-full">
-        <div className="w-full">
-          <Link href={"/"}>
-            <Image
-              src={"/horizontal.svg"}
-              alt="Logo"
-              width={110}
-              height={40}
-              className="pointer-events-none h-[40px] w-[110px]"
-            />
-          </Link>
+    <SectionWrapper id="developer">
+      <div>
+        <div className="flex flex-col gap-[18px] mb-[54px]">
+          <H1>Lorem ipsum dolor sit amet consectetur</H1>
+          <P>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam feugiat
+            urna quis eleifend pharetra. Fusce suscipit magna quis ligula
+            commodo, vestibulum sodales orci tempor. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Duis dapibus quis enim nec auctor.
+            Donec rhoncus dui sit amet augue placerat, molestie consequat nulla
+            tempus.
+          </P>
         </div>
-        <div className="relative w-full"></div>
-      </section>
-      <section></section>
-    </>
+
+        <div className="flex flex-wrap gap-x-[38px] gap-y-[62px]">
+          {devs.map((dev)=>(<DeveloperFigure dev={dev} key={dev.name}/>))}
+        </div>
+      </div>
+    </SectionWrapper>
   );
 }
