7672bb5bf7c7ca095f074774eddf9626808e75fc
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-03-25 10:18:48 +0700

    fix(frontend): Fix some stuff

--- src/app/(main)/berita/_components/parts/PageNav.tsx
+++ src/app/(main)/berita/_components/parts/PageNav.tsx
@@ -37,7 +37,7 @@ export default function PageNav({
               <EclipseIcon className="w-[6px] h-[6px]" />
             </div>
           ) : (
-            <TertiaryLinkButton href={"?page=" + num.toString()} key={num}>
+            <TertiaryLinkButton scroll={false} href={"?page=" + num.toString()} key={num}>
               {num}
             </TertiaryLinkButton>
           ),
