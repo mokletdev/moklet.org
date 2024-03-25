ba4279d3fbba0529139c4223f6abb4f8fe8806f1
Ahsan <ahsanaz461@gmail.com>
2024-03-25 08:08:17 +0700

    refactor: Use let instead of var

--- src/app/(form)/form/[id]/alreadysubmit/page.tsx
+++ src/app/(form)/form/[id]/alreadysubmit/page.tsx
@@ -10,25 +10,23 @@ export const metadata: Metadata = {
 
 export default function AlreadySubmit() {
   return (
-    <>
-      <main className="mx-auto w-full max-w-[1192px] px-5 py-[22px]">
-        <SectionWrapper id="not-found">
-          <div className="flex flex-col justify-center items-center gap-20">
-            <Image
-              src={"/images/Forbidden.svg"}
-              alt={"404"}
-              width={460}
-              height={244}
-            />
-            <div className="flex flex-col items-center justify-center">
-              <div className="mb-11 text-center">
-                <H2 className="mb-[18px]">Jawaban Anda telah terkirim</H2>
-              </div>
-              <PrimaryLinkButton href="/">Kembali ke beranda</PrimaryLinkButton>
+    <main className="mx-auto w-full max-w-[1192px] px-5 py-[22px]">
+      <SectionWrapper id="not-found">
+        <div className="flex flex-col justify-center items-center gap-20">
+          <Image
+            src={"/images/Forbidden.svg"}
+            alt={"404"}
+            width={460}
+            height={244}
+          />
+          <div className="flex flex-col items-center justify-center">
+            <div className="mb-11 text-center">
+              <H2 className="mb-[18px]">Jawaban Anda telah terkirim</H2>
             </div>
+            <PrimaryLinkButton href="/">Kembali ke beranda</PrimaryLinkButton>
           </div>
-        </SectionWrapper>
-      </main>
-    </>
+        </div>
+      </SectionWrapper>
+    </main>
   );
 }
